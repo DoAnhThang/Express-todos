ffbdbf14bbcc835d82a708a9ff3b989d8c363567
Glitch (efficient-puffy-layer) <none>
2020-05-05 14:20:58 +0000

    🌎👫 Updated with Glitch

--- server.js
+++ server.js
@@ -3,36 +3,15 @@
 
 // we've started you off with Express (https://expressjs.com/)
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.set('view engine', 'pug');
-app.set('views', './views');
-
-var todosList = [
-    {id:1,name:'đi chợ'},
-    {id:2,name:'nấu cơm'},
-    {id:3,name:'rửa bát'},
-    {id:4,name:'học code tại CodersX'}
-]
-
 // https://expressjs.com/en/starter/basic-routing.html
-app.get('/', (req, res) => {
-  res.render('index1',{
-        todos:todosList
-    });
+app.get("/", (request, response) => {
+  response.send(
+    "<ul><li>Đi chợ</li><li>Nấu cơm</li><li>Rửa bát</li><li>Học code tại CodersX</li></ul>"
+  );
 });
-
-
-app.get('/todos',function(req,res){
-    var q = req.query.q;
-    var matchTodo = todosList.filter(function(todo){
-       return todo.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
-    })
-    res.render('index1',{
-      todos:matchTodo
-    })
-})
 
 // listen for requests :)
 app.listen(process.env.PORT, () => {
